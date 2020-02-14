6c270f8c7b3d4ce6f93ac21a6ecc9ee66a5878cd
Justine Woo <justinewoo8@gmail.com>
2020-02-14 13:47:33 -0800

    hi

--- crawler/worker.py
+++ crawler/worker.py
@@ -2,10 +2,12 @@ from threading import Thread
 
 from utils.download import download
 from utils import get_logger
-from scraper import scraper
+from scraper import scraper, extract_text
 import time
 from urllib import robotparser
 from urllib.parse import urlparse
+from simhash import Simhash
+import re
 
 
 class Worker(Thread):
@@ -13,41 +15,166 @@ class Worker(Thread):
         self.logger = get_logger(f"Worker-{worker_id}", "Worker")
         self.config = config
         self.frontier = frontier
+
+        self.stop_words = set() #set of english stop words
+        self.robot_dict = {} #?????
+        self.simhashes = set() #simhash object
+        self.tokens = dict() #key = word, value = number of instances
+        self.subdomains = dict() #key = subdomain, value = number of paths
+        self.pagecount = 0
+        self.downloadcount = 0
+        self.longest = ['', 0]
+
         super().__init__(daemon=True)
-        
+    
     def run(self):
+        self.add_stop_words()
         while True:
             tbd_url = self.frontier.get_tbd_url()
-            #print(tbd_url)
             if not tbd_url:
                 self.logger.info("Frontier is empty. Stopping Crawler.")
+                self.print_output()
                 break
-            try:
-                parsed = urlparse(tbd_url)
-                robot = robotparser.RobotFileParser()
-                robot.set_url(parsed.scheme + "://" + parsed.netloc + "/robots.txt")
-                robot.read()
+                
+            parsed = urlparse(tbd_url)
+            #ALLOWED TO VISIT???
+            if self.robot(tbd_url).default_entry == None or (self.robot(tbd_url).default_entry != None and self.robot(tbd_url).can_fetch("*", tbd_url)):
+                resp = download(tbd_url, self.config, self.logger)
 
-                #print('robot read')
-                if robot.can_fetch("*", tbd_url):
-                    #print("robot says yes")
-                    resp = download(tbd_url, self.config, self.logger)
-                    self.logger.info(
-                        f"Downloaded {tbd_url}, status <{resp.status}>, "
-                        f"using cache {self.config.cache_server}.")
 
-                    if (resp.status == 200 or resp.status == 301 or resp.status == 302 or resp.status == 307):
-                        scraped_urls = scraper(tbd_url, resp)
-                        for scraped_url in scraped_urls:
-                            self.frontier.add_url(scraped_url)
+                text = extract_text(tbd_url, resp)
 
-                    self.frontier.mark_url_complete(tbd_url)
-                    if robot.crawl_delay("*"):
-                        time.sleep(robot.crawl_delay("*"))
-                    time.sleep(self.config.time_delay)
+                self.logger.info(
+                    f"Downloaded {tbd_url}, status <{resp.status}>, "
+                    f"using cache {self.config.cache_server}.")
+                self.downloadcount += 1
+
+
+
+                #bool for if the page has content
+                if not text:
+                    content = False
                 else:
-                    print("robot says no")
-            except:
-                pass
+                    text_list = text.split("\n")
+                    word_count = self.word_count(text_list)
+                    content = self.is_content(text, word_count)
+                print("good url?: ", content)
 
-            
+                #bool for if the page is text/html
+                try:
+                    is_text = resp.raw_response.headers['Content-Type'].replace(" ", "").lower() == 'text/html;charset=utf-8' or resp.raw_response.headers['Content-Type'].strip(" ").lower() == 'text/html;charset=ascii'
+                    
+                except:
+                    is_text = False;
+                print("is_text?", is_text)
+
+                #GOOD WEBSITE DO STUFF
+                if ((resp.status == 200 or resp.status == 301 or resp.status == 302 or resp.status == 307) and is_text and content):
+                    scraped_urls = scraper(tbd_url, resp)
+                    for scraped_url in scraped_urls:
+                        self.frontier.add_url(scraped_url)
+
+
+                    
+
+                    self.add_tokens(text_list)
+                    self.count_subdomain(tbd_url)
+                    self.pagecount += 1
+                    self.check_longest(tbd_url, word_count)
+                    #print(self.tokens)
+                    #print(self.subdomains)
+
+
+                #DO EVEN IF BAD WEBSITE
+                self.frontier.mark_url_complete(tbd_url)
+                if self.robot(tbd_url).default_entry != None and self.robot(tbd_url).crawl_delay("*"):
+                    time.sleep(self.robot(tbd_url).crawl_delay("*"))
+                time.sleep(self.config.time_delay)
+            else:
+                print("robot says no")
+
+    def robot(self, url):
+        parsed = urlparse(url)
+        if parsed.netloc not in self.robot_dict:
+            robot_obj = robotparser.RobotFileParser()
+            robot_resp = download(parsed.scheme + "://" + parsed.netloc + "/robots.txt", self.config, self.logger)
+            if robot_resp.raw_response != None:
+                robot_obj.parse(robot_resp.raw_response.content.decode().split("\n"))
+            self.robot_dict[parsed.netloc] = robot_obj
+
+        return self.robot_dict[parsed.netloc]
+
+
+    def is_content(self, text, word_count):
+        if text and word_count >= 150:
+            current_sim = Simhash(text)
+
+            #first link, nothing in simhash set
+            if len(self.simhashes) == 0:
+                self.simhashes.add(current_sim)
+                return True
+
+            for x in self.simhashes:
+                if current_sim.distance(x) <= 3:
+                    print("duplicate detected")
+                    return False
+            self.simhashes.add(current_sim)
+            return True
+        else:
+            print("low text count")
+            return False
+
+    def count_subdomain(self, url):
+        parsed = urlparse(url)
+        #check if its a ics.uci.edu subdomain
+        if parsed.netloc.find("ics.uci.edu") != -1:
+            #check if we've seen this subdomain before
+            if parsed.netloc in self.subdomains:
+                self.subdomains[parsed.netloc] += 1
+            else:
+                self.subdomains[parsed.netloc] = 1
+
+    def add_stop_words(self):
+        with open("stop_words.txt", encoding = 'utf-8') as infile:
+            for x in infile:
+                self.stop_words.add(x.strip())
+        
+
+    def add_tokens(self, token_list):
+        for token in token_list:
+            new = token.strip(" ").strip(",").strip().lower()
+            if (new not in self.stop_words) and (new != "") and (len(new) > 1) and not re.match("^[0-9]+$", new):
+                count = self.tokens.get(new, 0)
+                self.tokens[new] = count + 1
+
+        # remove non-ascii tokens
+
+    def word_count(self, token_list):
+        count = len(token_list)
+        print("word count ", count)
+        return count
+
+    def check_longest(self, url, word_count):
+        if word_count > self.longest[1]:
+            self.longest[0] = url
+            self.longest[1] = word_count
+
+    def print_output(self):
+        print("Number of Unique Pages:", self.pagecount)
+        print("Number of Pages Downloaded:", self.downloadcount)
+        print("Longest Page:", self.longest[0], "Word Count:", self.longest[1])
+        print("ics.uci.edu subdomains:")
+        for k, v in sorted(self.subdomains.items(), key=lambda x: -x[1]):
+            print(k, " = ", v)
+
+        print("////////////////////////////////////////////////////////////////////////////////")
+        print("Top 50 Words to Study and Chill To")
+        i = 1
+        for k, v in sorted(self.tokens.items(), key=lambda x: -x[1]):
+            print(k, " = ", v)
+            i += 1
+            if i > 50:
+                break
+
+
+
--- scraper.py
+++ scraper.py
@@ -3,18 +3,17 @@ from lxml import etree, html
 from io import StringIO, BytesIO
 from urllib.parse import urlparse
 from urllib import robotparser
+from bs4 import BeautifulSoup
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
 
 def extract_next_links(url, resp):
-    # Implementation requred.
-    #print(url, "**************************")
     out = []
     parser = etree.HTMLParser(encoding='UTF-8')
     try:
-        if resp.raw_response and (resp.status != 404) and (200 <= resp.status <= 599) :
+        if resp.raw_response:
             tree = etree.parse(StringIO(resp.raw_response.content.decode(encoding='UTF-8')), parser)
             tags = (tree.xpath("//a"))
             #links = all tags
@@ -24,28 +23,55 @@ def extract_next_links(url, resp):
                     temp = tag.attrib['href']
                     #temp is a string containing all urls in a file
                     for i in temp.split():
+                        no_frag = (i.split('#'))[0]
                         parsed = urlparse(url)
-                        #print(parsed.scheme + "://" + parsed.netloc + "/robots.txt")
-                        if i[0] == "/" and len(i) > 1 and i[1] != "/":
-                            out.append(parsed.scheme + "://" + parsed.netloc + i)
-                        elif parsed.fragment:
-                            print(parsed.fragment)
-                            out.append(i.split('#')[0])
-                        else:
-                            out.append(i)
-    except:
+
+                        # handles the 2 url types: path and full url
+                        if no_frag != "":
+                            if no_frag[0] == "/" and len(no_frag) > 1 and no_frag[1] != "/":
+                                out.append(parsed.scheme + "://" + parsed.netloc + no_frag)   
+                            else:
+                                out.append(no_frag)
+
+    except Exception as e:
+        print(e) 
         pass
     return out
+
+def extract_text(url, resp):
+    try:
+         if resp.raw_response:
+            web_content = StringIO(resp.raw_response.content.decode(encoding='UTF-8'))
+            soup = BeautifulSoup(web_content, "html.parser")
+            count = 0
+
+            for script in soup(["script", "style"]):
+                script.decompose()
+
+            text = soup.get_text()
+
+            lines = (line.strip() for line in text.splitlines())
+            chunks = (phrase.strip() for line in lines for phrase in line.split(" "))
+            
+            text = '\n'.join(chunk for chunk in chunks if chunk)
+            return text
+
+    except Exception as e:
+        print(e)
+
+
 def is_valid(url):
     try:
         parsed = urlparse(url)
         if (parsed.scheme not in set(["http", "https"])) or not re.match(r".*\.(ics.uci.edu|cs.uci.edu|informatics.uci.edu|stat.uci.edu|today.uci.edu/department/information_computer_sciences)", parsed.netloc.lower()):
             return False
+        if ("?share=" in url) or ("replytocom=" in url) or ("?ical=" in url):
+            return False
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
             + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
-            + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
+            + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names|ppsx"
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
